3bcfeba84b1910a1294f1ab4684cb68b897b4509
Samir Alexander Conde Pabón <rimas40414243444546@gmail.com>
2024-06-15 17:34:18 -0500

    Esto permite iniciar el proyecto

--- src/main/java/com/ufps/proyecto/ProyectoApplication.java
+++ src/main/java/com/ufps/proyecto/ProyectoApplication.java
@@ -2,10 +2,11 @@ package com.ufps.proyecto;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class ProyectoApplication {
-	
+
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectoApplication.class, args);
 	}
