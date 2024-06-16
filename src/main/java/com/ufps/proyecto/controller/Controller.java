02a21457eb23099a8af9c6c876f2c12b10ddc831
Nefer Sneyder <nefersneyderrp@ufps.edu.co>
2024-06-15 19:02:34 -0500

    Primeros cambios

---
+++ src/main/java/com/ufps/proyecto/controller/Controller.java
@@ -0,0 +1,28 @@
+package com.ufps.proyecto.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+@org.springframework.stereotype.Controller
+public class Controller {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/eventos")
+    public String eventos() {
+        return "eventos";
+    }
+
+    @GetMapping("/merch")
+    public String merch() {
+        return "merch";
+    }
+
+    @GetMapping("/contacto")
+    public String contacto() {
+        return "contacto";
+    }
+}
